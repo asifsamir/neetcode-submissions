fa3c3af7e22e8aa57aa9c836e54dfda028f9493d
Asif Samir <64280545+asifsamir@users.noreply.github.com>
2026-04-17 23:03:38 -0300

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # unique_nums = set(numbers)
+
+        # indices_return = []
+        # for index, num in enumerate(numbers):
+        #     to_get = target - num
+
+        #     if to_get in unique_nums:
+        #         r_ind = numbers.index(to_get, index+1)
+        #         indices_return.extend([index+1, r_ind+1])
+
+        #         return indices_return
+
+        Li = 0
+        Ri = len(numbers) - 1
+
+        indices_return = []
+        while Li < Ri:
+            temp = numbers[Li] + numbers[Ri]
+
+            if temp == target:
+                return [Li+1, Ri+1]
+            elif temp > target and Ri > -1:
+                while temp > target:
+                    Ri -= 1
+                    temp = numbers[Li] + numbers[Ri]
+            else:
+                while temp < target and Li < len(numbers):
+                    Li += 1
+                    temp = numbers[Li] + numbers[Ri]
+
+
+
